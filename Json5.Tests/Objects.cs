f6ae075e10f93d0a605783a6250b08b93b07405a
bert2 <shuairan@gmail.com>
2024-12-23 23:08:43 +0100

    🐛 improve identifer parser error messages

--- Json5.Tests/Objects.cs
+++ Json5.Tests/Objects.cs
@@ -17,11 +17,14 @@ public class Objects {
     void EncodedInvalidAsciiInMemberName() => Parser.Parse(@"{a\u005Cb:1}").Should().BeValue("");
 
     [Fact]
-    void EncodedInvalidAsciiInMemberName2() => Parser.Parse(@"{a\u005Cb\u0061:1}").Should().BeValue("");
+    void EncodedInvalidAsciiInMemberName2() => Parser.Parse(@"{a\u0061b\u0062\u005Cc\u0063:1}").Should().BeValue("");
+
+    [Fact]
+    void EncodedInvalidAsciiInMemberName3() => Parser.Parse(@"{a\u{061}b\u{00062}\u{5C}c\u{0063}:1}").Should().BeValue("");
 
     [Fact]
     void InvalidUnicodeInMemberName() => Parser.Parse("{a\u200Ab:1}").Should().BeValue("");
 
     [Fact]
-    void InvalidEncodedUnicodeInMemberName() => Parser.Parse(@"{a\u200Db:1}").Should().BeValue("");
+    void InvalidEncodedUnicodeInMemberName() => Parser.Parse(@"{a\u200Ab:1}").Should().BeValue("");
 }
--- Json5/Internal/ObjectParser.cs
+++ Json5/Internal/ObjectParser.cs
@@ -10,6 +10,7 @@ using FParsec;
 using FParsec.CSharp;
 
 using System.Text;
+using System.Text.RegularExpressions;
 
 using static CommonParsers;
 using static FParsec.CSharp.CharParsersCS;
@@ -19,7 +20,7 @@ using static StringParser;
 using Chars = FParsec.CharStream<Unit>;
 using JsonNodeP = FSharpFunc<FParsec.CharStream<Unit>, FParsec.Reply<JsonNode?>>;
 
-public static class ObjectParser {
+public static partial class ObjectParser {
     public static JsonNodeP Json5Object { get; set; }
 
     static ObjectParser() {
@@ -30,9 +31,9 @@ public static class ObjectParser {
 
         var identifierChar = NoneOf($"\\:{WhitespaceChars}").Lbl_("identifier character");
 
-        var identifier1stPassP = Many1Strings(Choice(escapeSequence, Many1Chars(identifierChar)));
+        var identifier1stPass = Many1Strings(Choice(escapeSequence, Many1Chars(identifierChar)));
 
-        var identifier2ndPassP = CharParsers
+        var identifier2ndPass = CharParsers
             .identifier<Unit>(new(
                 isAsciiIdStart: FSharpFunc.From((char c) => c is '$' or '_' || char.IsLetter(c)),
                 isAsciiIdContinue: FSharpFunc.From((char c) => c is '$' or '_' || char.IsLetterOrDigit(c)),
@@ -50,24 +51,26 @@ public static class ObjectParser {
         // 1st pass: accept basically anything while also replacing unicode escape sequences.
         // 2nd pass: use FParsec's identifier parser on the result of the 1st pass for validation and unicode magic.
         var identifier = FSharpFunc.From((Chars chars1) => {
-            var reply1 = identifier1stPassP.Invoke(chars1);
+            var startIdx = chars1.Index;
+            var reply1 = identifier1stPass.Invoke(chars1);
             if (reply1.Status != ReplyStatus.Ok) return reply1;
 
-            var inputLen = reply1.Result.Length;
-            var chars2 = new Chars(reply1.Result, 0, inputLen);
-            var reply2 = identifier2ndPassP.Invoke(chars2);
+            var chars2 = new Chars(reply1.Result, 0, reply1.Result.Length);
+            var reply2 = identifier2ndPass.Invoke(chars2);
             if (reply2.Status == ReplyStatus.Ok) return reply2;
 
-            // Rewind original stream to the position where the validation error occured.
-            chars1.Skip(-inputLen + chars2.Index);
+            // Rewind original stream to the position where identifer parsing started.
+            chars1.Seek(startIdx);
+            // Now skip ahead to where the validation error occured while correctly identifying escape sequences.
+            chars1.SkipEscaped(chars2.Index);
 
-            var err = chars1.Peek() == chars2.Peek()
-                ? reply2.Error
-                : new(new ErrorMessage.Message(
-                    "The unicode escape sequence ending at the indicated position was replaced with the invalid character "
-                    + $"'{chars2.Peek()}'. The full identifier string after replacing all escape sequences was: '{reply1.Result}'."));
+            var err = chars1.MatchUnicodeEscape(out _)
+                ? new(new ErrorMessage.Message(
+                    "The unicode escape sequence starting at the indicated position was replaced with the invalid character "
+                    + $"'{chars2.Peek()}'. The full identifier string after replacing all escape sequences was: '{reply1.Result}'."))
+                : reply2.Error;
 
-            return new Reply<string>(reply2.Status, err);
+        return new Reply<string>(reply2.Status, err);
         }).Lbl_("identifier");
 
         var memberName = Choice(identifier, RawString);
@@ -85,4 +88,30 @@ public static class ObjectParser {
             .Map(props => (JsonNode?)new JsonObject(props))
             .Lbl("object");
     }
+
+    public static void SkipEscaped(this Chars chars, long utf16Offset) {
+        if (utf16Offset < 0) throw new ArgumentOutOfRangeException(nameof(utf16Offset), "Must be positive.");
+
+        for(; utf16Offset > 0; utf16Offset--)
+            chars.Skip(chars.MatchUnicodeEscape(out var len) ? len : 1);
+    }
+
+    public static bool MatchUnicodeEscape(this Chars chars, out int length) {
+        if (chars.Match(Utf16Escape()).Success) {
+            length = 6;
+            return true;
+        } else if (chars.Match(Utf32Escape()) is { Success: true, Length: var len }) {
+            length = len;
+            return true;
+        } else {
+            length = 0;
+            return false;
+        }
+    }
+
+    [GeneratedRegex(@"^\\u[\da-fA-F]{4}")]
+    private static partial Regex Utf16Escape();
+
+    [GeneratedRegex(@"^\\u\{[\da-fA-F]+\}")]
+    private static partial Regex Utf32Escape();
 }
