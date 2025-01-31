0c9104eb762bbf228cbf8e4941ebb900d2fa21bd
bert2 <shuairan@gmail.com>
2025-01-31 17:24:47 +0100

    👷‍♀️ remove semver parameter

--- build/Build.cs
+++ build/Build.cs
@@ -12,52 +12,47 @@ using static Nuke.Common.Tools.DotNet.DotNetTasks;
 class Build : NukeBuild {
     public static int Main() => Execute<Build>(x => x.Compile);
 
-    [GitRepository]
-    readonly GitRepository Repository = null!;
-
-    [PathVariable]
-    readonly Tool Git = null!;
-
-    [Solution(GenerateProjects = true)]
-    readonly Solution Solution = null!;
+    [GitRepository] readonly GitRepository Repository = null!;
+    [PathVariable] readonly Tool Git = null!;
+    [Solution(GenerateProjects = true)] readonly Solution Solution = null!;
 
     [Parameter($"NuGet API key - Required for target {nameof(Publish)}"), Secret]
     readonly string NugetApiKey = null!;
 
+    string? lastGitTag;
+    string LastGitTag => lastGitTag ??= Git("describe --tags --abbrev=0", logOutput: false).Single().Text;
+    bool LastCommitHasVersionTag => Repository.Tags.Contains(LastGitTag) && Version.TryParse(LastGitTag, out var _);
+
     AbsolutePath ArtifactsDir => RootDirectory / "artifacts";
+    AbsolutePath PackagePath => ArtifactsDir / $"{Solution.Json5.Name}.{LastGitTag}.nupkg";
 
-    AbsolutePath PackagePath => ArtifactsDir / $"{Solution.Json5.Name}.{LatestGitTag}.nupkg";
-
-    string? latestGitTag;
-    string LatestGitTag => latestGitTag ??= Git("describe --tags --abbrev=0", logOutput: false).Single().Text;
-
-    Target Compile => _ => _
+    Target Compile => t => t
         .Executes(() => DotNetBuild(opts => opts
             .SetConfiguration("Release")
-            .SetVersion(LatestGitTag)));
+            .SetVersion(LastGitTag)));
 
-    Target Test => _ => _
+    Target Test => t => t
         .DependsOn(Compile)
         .Executes(() => DotNetTest(opts => opts
             .SetProjectFile(Solution.Json5_Tests)
             .SetNoBuild(true)));
 
-    Target Pack => _ => _
+    Target Pack => t => t
         .DependsOn(Compile)
         .Executes(() => DotNetPack(opts => opts
             .SetProject(Solution.Json5)
             .SetNoBuild(true)
-            .SetVersion(LatestGitTag)
+            .SetVersion(LastGitTag)
             .SetOutputDirectory(ArtifactsDir)));
 
-    Target Publish => _ => _
+    Target Publish => t => t
         .DependsOn(Test, Pack)
         .Requires(() => Repository.IsOnMainBranch())
-        .Requires(() => Repository.Tags.Contains(LatestGitTag))
+        .Requires(() => LastCommitHasVersionTag)
         .Requires(() => NugetApiKey)
         .Executes(() => /*DotNetNuGetPush(opts => opts
             .SetTargetPath(PackagePath)
             .SetSource("https://www.nuget.org/")
             .SetApiKey(NugetApiKey)));*/
-            Log.Information($"dotnet nuget push {PackagePath} --source https://www.nuget.org/ --api-key {NugetApiKey}"));
+            Log.Information($"{string.Join(",", Repository.Tags)} dotnet nuget push {PackagePath} --source https://www.nuget.org/ --api-key {NugetApiKey}"));
 }
