f590cbf735292458784d48081976761457d8c1ad
bert2 <shuairan@gmail.com>
2025-01-31 17:01:54 +0100

    👷‍♀️ create ci pipeline

--- build/Build.cs
+++ build/Build.cs
@@ -1,43 +1,63 @@
-using System;
-using System.Linq;
 using Nuke.Common;
-using Nuke.Common.CI;
-using Nuke.Common.Execution;
+using Nuke.Common.Git;
 using Nuke.Common.IO;
 using Nuke.Common.ProjectModel;
 using Nuke.Common.Tooling;
-using Nuke.Common.Utilities.Collections;
-using static Nuke.Common.EnvironmentInfo;
-using static Nuke.Common.IO.PathConstruction;
+using Nuke.Common.Tools.DotNet;
 
-class Build : NukeBuild
-{
-    /// Support plugins are available for:
-    ///   - JetBrains ReSharper        https://nuke.build/resharper
-    ///   - JetBrains Rider            https://nuke.build/rider
-    ///   - Microsoft VisualStudio     https://nuke.build/visualstudio
-    ///   - Microsoft VSCode           https://nuke.build/vscode
+using Serilog;
 
-    public static int Main () => Execute<Build>(x => x.Compile);
+using static Nuke.Common.Tools.DotNet.DotNetTasks;
 
-    [Parameter("Configuration to build - Default is 'Debug' (local) or 'Release' (server)")]
-    readonly Configuration Configuration = IsLocalBuild ? Configuration.Debug : Configuration.Release;
+class Build : NukeBuild {
+    public static int Main() => Execute<Build>(x => x.Compile);
 
-    Target Clean => _ => _
-        .Before(Restore)
-        .Executes(() =>
-        {
-        });
+    [GitRepository]
+    readonly GitRepository Repository = null!;
 
-    Target Restore => _ => _
-        .Executes(() =>
-        {
-        });
+    [PathVariable]
+    readonly Tool Git = null!;
+
+    [Solution(GenerateProjects = true)]
+    readonly Solution Solution = null!;
+
+    [Parameter($"NuGet API key - Required for target {nameof(Publish)}"), Secret]
+    readonly string NugetApiKey = null!;
+
+    AbsolutePath ArtifactsDir => RootDirectory / "artifacts";
+
+    AbsolutePath PackagePath => ArtifactsDir / $"{Solution.Json5.Name}.{LatestGitTag}.nupkg";
+
+    string? latestGitTag;
+    string LatestGitTag => latestGitTag ??= Git("describe --tags --abbrev=0", logOutput: false).Single().Text;
 
     Target Compile => _ => _
-        .DependsOn(Restore)
-        .Executes(() =>
-        {
-        });
+        .Executes(() => DotNetBuild(opts => opts
+            .SetConfiguration("Release")
+            .SetVersion(LatestGitTag)));
 
+    Target Test => _ => _
+        .DependsOn(Compile)
+        .Executes(() => DotNetTest(opts => opts
+            .SetProjectFile(Solution.Json5_Tests)
+            .SetNoBuild(true)));
+
+    Target Pack => _ => _
+        .DependsOn(Compile)
+        .Executes(() => DotNetPack(opts => opts
+            .SetProject(Solution.Json5)
+            .SetNoBuild(true)
+            .SetVersion(LatestGitTag)
+            .SetOutputDirectory(ArtifactsDir)));
+
+    Target Publish => _ => _
+        .DependsOn(Test, Pack)
+        .Requires(() => Repository.IsOnMainBranch())
+        .Requires(() => Repository.Tags.Contains(LatestGitTag))
+        .Requires(() => NugetApiKey)
+        .Executes(() => /*DotNetNuGetPush(opts => opts
+            .SetTargetPath(PackagePath)
+            .SetSource("https://www.nuget.org/")
+            .SetApiKey(NugetApiKey)));*/
+            Log.Information($"dotnet nuget push {PackagePath} --source https://www.nuget.org/ --api-key {NugetApiKey}"));
 }
